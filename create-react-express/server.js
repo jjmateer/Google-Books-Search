a300188f52c31a7376fcd9c4c099b3e35f41c21a
John Mateer <jj.mateer@live.com>
2019-10-25 14:56:14 -0700

    Created general layout and connected to mongodb.

---
+++ create-react-express/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// const router = require("router");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+//Connect to mongoose
+var dbUrl = `mongodb+srv://jjmateer:${process.env.MONGO_PW}@cluster0-q0kab.mongodb.net/googlebooks?retryWrites=true&w=majority`;mongoose.connect(dbUrl, err => {
+  console.log("Connected to mongoose");
+  if (err) {
+    console.log(err)
+  }
+});
+// app.use("/", router);
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
