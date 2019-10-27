a40d4fe2ccd848273378cf8bc3d12978c06a6639
John Mateer <jj.mateer@live.com>
2019-10-27 13:11:32 -0700

    Start fresh, deleted all components.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-// import Header from "../components/Header/Header.js";
 import Banner from "../components/Banner/Banner.js";
 import Search from "../components/Search/Search.js";
-// import Results from "../components/Results/Results.js";
+import Results from "../components/Results/Results.js";
 import Book from "../components/Book/Book.js";
-import Card from "../components/Card/Card"
-import { List } from "../components/List/List"
 import API from "../API.js";
+// import Container from "../components/Results/Results";
 
 class Home extends Component {
     state = {
         books: [],
         q: "",
         message: "Search For A Book To Begin!"
+    };
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getBooks();
     };
     getBooks = () => {
         API.getBooks(this.state.q)
@@ -28,16 +36,6 @@ class Home extends Component {
                 })
             );
     };
-    handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    };
-    handleFormSubmit = event => {
-        event.preventDefault();
-        this.getBooks();
-    };
     handleBookSave = id => {
         const book = this.state.books.find(book => book.id === id);
 
@@ -52,37 +50,6 @@ class Home extends Component {
         }).then(() => this.getBooks());
     };
     render() {
-        return (
-            <div>
-                <Banner />
-                <Search
-                    handleInputChange={this.handleInputChange}
-                    handleFormSubmit={this.handleFormSubmit}
-                    q={this.state.q}
-                />
-                <Card>
-                    <List>
-                        {this.state.books.map(book => (
-                            <Book
-                                key={book.id}
-                                title={book.volumeInfo.title}
-                                subtitle={book.volumeInfo.subtitle}
-                                link={book.volumeInfo.infoLink}
-                                authors={book.volumeInfo.authors.join(", ")}
-                                description={book.volumeInfo.description}
-                                image={book.volumeInfo.imageLinks.thumbnail}
-                                Button={() => (
-                                    <button
-                                        onClick={() => this.handleBookSave(book.id)}
-                                        className="btn btn-primary ml-2">Save</button>
-                                )}
-                            />
-                        ))}
-                    </List>
-                </Card>
-
-            </div>
-        )
     }
 }
 
