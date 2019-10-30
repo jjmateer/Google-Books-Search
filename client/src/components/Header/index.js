a4286b64babbdd861e3e95fd4a95d464f338e014
John Mateer <jj.mateer@live.com>
2019-10-30 15:33:53 -0700

    Added conditional activation of bootstrap nav links.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -10,11 +10,11 @@ function Header() {
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                    <li className="nav-item">
+                        <a className={window.location.pathname === "/" ? "nav-link active" : "nav-link"} href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="saved">Saved</a>
+                        <a className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"} href="saved">Saved</a>
                     </li>
                 </ul>
             </div>
